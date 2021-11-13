27682f8ad04cb633db6f6d2b183ade3c0bbd14af
alexandr1983 <alexandr.mullin@gmail.com>
2021-11-13 15:25:42 +0300

    Create 1.1.py

---
+++ 1.1.py
@@ -0,0 +1,10 @@
+a = "How mach is the"
+b = "fish?"
+print(a, b)
+n1 = int(input("Enter first number: "))
+n2 = int(input("Enter second number: "))
+a = input("Enter your name: ")
+if n1 > n2:
+    print(f"{a}, number {n1} is greater than {n2}")
+else:
+    print(f"{a}, number {n1} is less than {n2}")
